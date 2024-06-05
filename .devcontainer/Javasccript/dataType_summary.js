0a63c88e18d61681e43e542edd1556a80a04c062
Salman-Bilal <137156683+Salman-Bilal@users.noreply.github.com>
2024-06-05 20:43:53 +0000

    summary

---
+++ .devcontainer/Javasccript/dataType_summary.js
@@ -0,0 +1,13 @@
+// Primitive Data Types
+
+// 7 Types : string, Number, Boolean, null, symbol, BigInt, undefined
+
+
+
+//Non Premitive
+// Array, Objects, Function
+
+let Id = Symbol('123')
+let anotherId = Symbol('123')
+
+console.log(Id === anotherId); //output: false 
