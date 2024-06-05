70ee4583f236fa60bd010085603ecda4c91ec590
Salman-Bilal <137156683+Salman-Bilal@users.noreply.github.com>
2024-06-05 18:57:14 +0000

    container

---
+++ .devcontainer/Javasccript/variable.js
@@ -0,0 +1,11 @@
+const accountId = 12312
+let accountEmail = "salman@gamil.com"
+var accountPasword = "21321"
+accountCity = "Lahore"
+
+/* 
+Prefer not to use var 
+Because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPasword, accountCity])
