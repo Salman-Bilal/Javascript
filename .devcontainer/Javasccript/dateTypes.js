298e8c35b1b2da8bb5972765a442aa85c942c8f1
Salman-Bilal <137156683+Salman-Bilal@users.noreply.github.com>
2024-06-05 19:25:16 +0000

    datatypes

---
+++ .devcontainer/Javasccript/dateTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are uisng node.js not browser
+
+// number -> 2 to power of 53
+// BigInt
+// String -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined
+// Symbol -> unique
+
+
+// object 
+
+console,log(typeof null) //object 
+console.log(undefined); //undefined 
