a05b734d38f32f5e327888660429c647ce09bad7
Salman-Bilal <137156683+Salman-Bilal@users.noreply.github.com>
2024-06-07 18:40:52 +0000

    math and num

---
+++ .devcontainer/Javasccript/num_math.js
@@ -0,0 +1,31 @@
+// const score = 100
+// console.log(score);
+
+// const balance = new Number(400)
+// console.log(balance);
+
+// console.log(balance.toString.length)
+// console.log(balance.toFixed(2))
+
+// const num2 = 123.8966
+// console.log(num2.toPrecision(4))
+
+// const num3 = 10000000 
+// console.log(num3.toLocaleString());
+
+
+//********************Math********************************/
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.6))
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10 + 1))
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + 10);
