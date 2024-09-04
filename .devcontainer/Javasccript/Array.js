a6582359ac3fe32bb58ee4831c13f241d39aa601
Salman-Bilal <137156683+Salman-Bilal@users.noreply.github.com>
2024-09-04 10:47:31 +0000

    array

---
+++ .devcontainer/Javasccript/Array.js
@@ -0,0 +1,21 @@
+//Array declaration mehtods
+
+// const myArr= [0,1,2,3,4,5]
+// const newArr= new Array(0,1,2,3,4,5)
+
+//Methods
+// myArr.push(6) -> to add value at the end
+//myArr.pop()  -> to remove value at end
+// shift()  -> to remove value form start
+// unshift(1,4) -> to start value from start
+// slice(1,3) -> in this method it show from 1 to 3 but in this 3 or third value is not include and not make any changes in array
+// splice(1,3) -> it shows value from 1 to 3 with inclding three and also cut the value from array or remove the from array from 1 to 3 or any given range.
+// array1.concat(array2) -> to combine two arrays
+// const newarray=array1.spread(multiple arrays) to combine multiplae arrays
+// syntax spread -> const newarr = [...array1, ...array2,....n]
+// array.flat[depth value -> 1,2,3 or infinity]
+//  means if we have an array like array= [1,2,3,[4,5],6,7,[8,9]]
+
+
+// console.log(Array.from("Salman")); -> from() used to convert string into array
+// Array.of() -> of() used to convert different variable values into array.
