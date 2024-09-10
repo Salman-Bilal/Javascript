63824c05db7df8fb731ed842df473ffcfc3d67a3
Salman-Bilal <137156683+Salman-Bilal@users.noreply.github.com>
2024-09-10 15:03:33 +0000

    while and dowhile loop

---
+++ .devcontainer/Javasccript/While_do_while_loop.js
@@ -0,0 +1,15 @@
+let value = 1
+
+// while (value <= 10) {
+
+//     console.log(value);
+//     value++;
+// }
+
+let number =1
+do {
+    if (number%5==0) {
+        console.log(`${number} is divisible by 5`);
+    }
+    number++
+} while (number <= 100);
