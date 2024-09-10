a8f0e84969cf1487497bc66d54825976323272fe
Salman-Bilal <137156683+Salman-Bilal@users.noreply.github.com>
2024-09-10 14:39:04 +0000

    for loop

---
+++ .devcontainer/Javasccript/For_Loop.js
@@ -0,0 +1,15 @@
+const array = ["Pakistan", "India", "Saudia Arab", "Dubai", "United States", "United Kingdom"]
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`Table of: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(i + " * " +j+ " = " + i*j);     
+        
+    }
+    
+}
